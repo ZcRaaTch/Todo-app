9e936e0793c742ff5923e0c49edfb3fa3453c077
Abhinav Awana <abhinavawana2444@gmail.com>
2025-02-19 01:05:43 +0530

    input design complete

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TodoForm() {
   return (
     <>
-      <div className="mx-auto my-6 flex gap-3 w-[85%] px-4 py-3.5 rounded-lg bg-[var(--color-light-primary)] dark:bg-[var(--color-dark-secondary-1)] ">
+      <div className="mx-auto mb-8 mt-10 flex gap-3 w-[85%] px-4 py-3.5 rounded-lg bg-[var(--color-light-primary)] dark:bg-[var(--color-dark-secondary-1)] ">
         <button className="w-6 h-6 rounded-full flex items-center justify-center border text-[1rem] font-bold border-[var(--color-light-secondary-3)] dark:border-[var(--color-dark-secondary-5)] shadow-sm shadow-indigo-500  ">
           +
         </button>
