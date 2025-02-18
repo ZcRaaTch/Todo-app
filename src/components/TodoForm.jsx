95e03be4f2c65167dbc111d1336fd31285ce2a67
Abhinav Awana <abhinavawana2444@gmail.com>
2025-02-19 01:00:06 +0530

    input design complete

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function TodoForm() {
   return (
     <>
-      <div className="mx-auto my-6 flex w-[85%] bg-[var(--color-light-primary)] dark:bg-[var(--color-dark-secondary-1)]">
-        <button>+</button>
+      <div className="mx-auto my-6 flex gap-3 w-[85%] px-4 py-3.5 rounded-lg bg-[var(--color-light-primary)] dark:bg-[var(--color-dark-secondary-1)] ">
+        <button className="w-6 h-6 rounded-full flex items-center justify-center border text-[1rem] font-bold border-[var(--color-light-secondary-3)] dark:border-[var(--color-dark-secondary-5)] shadow-sm shadow-indigo-500  ">
+          +
+        </button>
         <input
           type="text"
           placeholder="Create a new todo..."
-          className="border-none outline-none"
+          className="border-none outline-none block w-full"
         />
       </div>
     </>
